4cc70ccabd7832c2942a8a9f7f1f160eb60f6a72
alejandro <alejandro.amendola@gmail.com>
2021-02-02 12:54:07 -0300

    Automatic commit message

---
+++ impl/elasticsearch.go
@@ -0,0 +1,71 @@
+package elasticsearch
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	utils "github.com/aamendola/go-utils"
+	"github.com/elastic/go-elasticsearch/v6"
+)
+
+// Elasticsearch ...
+type Elasticsearch struct {
+	uri string
+}
+
+type messageUpdate struct {
+	Doc doc `json:"doc"`
+}
+
+type doc struct {
+	Content string `json:"content"`
+}
+
+// NewElasticsearch ...
+func NewElasticsearch(host, port, index string) (*Elasticsearch, error) {
+
+	if len(host) == 0 || len(port) == 0 || len(index) == 0 {
+		return nil, fmt.Errorf("Eslastic configuration error")
+	}
+
+	elastic := Elasticsearch{
+		uri: fmt.Sprintf("http://%v:%v", host, port),
+	}
+
+	return &elastic, nil
+}
+
+// Update ...
+func (e Elasticsearch) Update(index, id, content string) (string, error) {
+
+	// ctx := context.Background()
+	// _ = ctx // ?????????????????????????????????????????
+
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			e.uri,
+		},
+		Transport: &http.Transport{Proxy: nil},
+	}
+
+	client, err := elasticsearch.NewClient(cfg)
+	utils.PanicOnError(err)
+
+	m := messageUpdate{
+		Doc: doc{
+			Content: content,
+		},
+	}
+
+	b, err := json.Marshal(m)
+	utils.PanicOnError(err)
+
+	update, err := client.Update(index, id, strings.NewReader(string(b)))
+	utils.PanicOnError(err)
+
+	log.Println(update)
+	return "", nil
+}
--- indexer.go
+++ indexer.go
@@ -1,85 +1,9 @@
 package indexer
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"strings"
+import "io"
 
-	utils "github.com/aamendola/go-utils"
-	"github.com/elastic/go-elasticsearch/v6"
-)
-
-// Elastic ...
-type Elastic struct {
-	index string
-	uri   string
-}
-
-// Document ...
-type Document struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Content     string `json:"content"`
-	SystemOwner string `json:"systemowner"`
-	UserOwner   string `json:"userowner"`
-	Path        string `json:"path"`
-}
-
-type messageUpdate struct {
-	Doc doc `json:"doc"`
-}
-
-type doc struct {
-	Content string `json:"content"`
-}
-
-// NewElastic ...
-func NewElastic(host, port, index string) (*Elastic, error) {
-
-	if len(host) == 0 || len(port) == 0 || len(index) == 0 {
-		return nil, fmt.Errorf("Eslastic configuration error")
-	}
-
-	elastic := Elastic{
-		index: index,
-		uri:   fmt.Sprintf("http://%v:%v", host, port),
-	}
-
-	return &elastic, nil
-}
-
-// UpdateDoc ...
-func (e Elastic) UpdateDoc(document Document) (string, error) {
-
-	ctx := context.Background()
-	_ = ctx // ?????????????????????????????????????????
-
-	cfg := elasticsearch.Config{
-		Addresses: []string{
-			e.uri,
-		},
-		Transport: &http.Transport{Proxy: nil},
-	}
-
-	client, err := elasticsearch.NewClient(cfg)
-	utils.PanicOnError(err)
-
-	m := messageUpdate{
-		Doc: doc{
-			Content: document.Content,
-		},
-	}
-
-	b, err := json.Marshal(m)
-	utils.PanicOnError(err)
-
-	update, err := client.Update(e.index, document.ID, strings.NewReader(string(b)))
-	utils.PanicOnError(err)
-
-	log.Println(update)
-	return "", nil
+// Indexer ...
+type Indexer interface {
+	Update(index, id string, body io.Reader)
+	// Get(id string) (document interface{})
 }
